06f3b6cff8c9c4b8a8284b795dfdcb1bd4ed27bf
Aarushi <aarushitarun18@gmail.com>
2024-09-23 20:05:12 +0530

    DSA

---
+++ SinglyLinkedList/project14/SinglyLinkedList.java
@@ -0,0 +1,62 @@
+package SinglyLinkedList.project14;
+
+public class SinglyLinkedList {
+    private ListNode head;
+
+    public static class ListNode{
+        private int data;
+        private ListNode next;
+
+        public ListNode(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public void display(){
+        ListNode current = head;
+        while(current != null){
+            System.out.print(current.data + " --> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+    public void insertLast(int value){
+        ListNode newNode = new ListNode(value);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        ListNode current = head;
+        while(null != current.next){
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    public ListNode getMiddleNode(){
+        if(head == null){
+            return null;
+        }
+
+        ListNode slowPtr = head;
+        ListNode fastPtr = head;
+
+        while(fastPtr != null && fastPtr.next != null){
+            slowPtr = slowPtr.next;
+            fastPtr = fastPtr.next.next;
+        }
+        return slowPtr;
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedList sll = new SinglyLinkedList();
+        sll.insertLast(11);
+        sll.insertLast(8);
+        sll.insertLast(1);
+        sll.insertLast(12);
+        sll.display();
+
+        ListNode middleNode = sll.getMiddleNode();
+        System.out.println("middle node is - " + middleNode.data);
+    }
+}
